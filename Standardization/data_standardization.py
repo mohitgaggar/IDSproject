cd725665a2eea91cdb5988bcb8d510f04d5fb93e
Mohitgaggar <gaggarmohit@gmail.com>
2019-11-15 13:24:40 +0530

    Data standardization done

---
+++ Standardization/data_standardization.py
@@ -0,0 +1,11 @@
+# Standardize the data attributes for the Iris dataset.
+# from sklearn.datasets import load_iris
+from sklearn import preprocessing
+import pandas as pd 
+import numpy as np
+# load the Iris dataset
+df = pd.read_csv("../Cleaning/car_cleaned.csv") 
+numerical=['Year','Engine HP','Engine Cylinders',	'Number of Doors'	,'highway MPG',	'city mpg',	'Popularity']
+df[numerical] = preprocessing.StandardScaler().fit_transform(df[numerical])
+
+df.to_csv('standardized_car.csv')  
